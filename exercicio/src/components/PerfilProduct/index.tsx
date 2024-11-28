30fbacbb8d5ef37906254c7d88b4c63995ef9bc8
Reinaldo <reinaldo18junior@gmail.com>
2024-11-28 14:43:59 -0300

    Começando a integrar com a API

--- exercicio/src/components/PerfilProduct/index.tsx
+++ exercicio/src/components/PerfilProduct/index.tsx
@@ -10,85 +10,100 @@ import {
 
 import pizza from '../../assets/images/pizza.png'
 
-const PerfilProduct = () => (
-  <Container>
-    <div className="container">
-      <List>
-        <li>
-          <Card>
-            <PizzaImg src={pizza} alt="Pizza" />
-            <PizzaName>Pizza Marguerita</PizzaName>
-            <Description>
-              A clássica Marguerita: molho de tomate suculento, mussarela
-              derretida, manjericão fresco e um toque de azeite. Sabor e
-              simplicidade!
-            </Description>
-            <Button href="#">Adicionar ao carrinho</Button>
-          </Card>
-        </li>
-        <li>
-          <Card>
-            <PizzaImg src={pizza} alt="Pizza" />
-            <PizzaName>Pizza Marguerita</PizzaName>
-            <Description>
-              A clássica Marguerita: molho de tomate suculento, mussarela
-              derretida, manjericão fresco e um toque de azeite. Sabor e
-              simplicidade!
-            </Description>
-            <Button href="#">Adicionar ao carrinho</Button>
-          </Card>
-        </li>
-        <li>
-          <Card>
-            <PizzaImg src={pizza} alt="Pizza" />
-            <PizzaName>Pizza Marguerita</PizzaName>
-            <Description>
-              A clássica Marguerita: molho de tomate suculento, mussarela
-              derretida, manjericão fresco e um toque de azeite. Sabor e
-              simplicidade!
-            </Description>
-            <Button href="#">Adicionar ao carrinho</Button>
-          </Card>
-        </li>
-        <li>
-          <Card>
-            <PizzaImg src={pizza} alt="Pizza" />
-            <PizzaName>Pizza Marguerita</PizzaName>
-            <Description>
-              A clássica Marguerita: molho de tomate suculento, mussarela
-              derretida, manjericão fresco e um toque de azeite. Sabor e
-              simplicidade!
-            </Description>
-            <Button href="#">Adicionar ao carrinho</Button>
-          </Card>
-        </li>
-        <li>
-          <Card>
-            <PizzaImg src={pizza} alt="Pizza" />
-            <PizzaName>Pizza Marguerita</PizzaName>
-            <Description>
-              A clássica Marguerita: molho de tomate suculento, mussarela
-              derretida, manjericão fresco e um toque de azeite. Sabor e
-              simplicidade!
-            </Description>
-            <Button href="#">Adicionar ao carrinho</Button>
-          </Card>
-        </li>
-        <li>
-          <Card>
-            <PizzaImg src={pizza} alt="Pizza" />
-            <PizzaName>Pizza Marguerita</PizzaName>
-            <Description>
-              A clássica Marguerita: molho de tomate suculento, mussarela
-              derretida, manjericão fresco e um toque de azeite. Sabor e
-              simplicidade!
-            </Description>
-            <Button href="#">Adicionar ao carrinho</Button>
-          </Card>
-        </li>
-      </List>
-    </div>
-  </Container>
-)
+// tentando tipar com a API, esse seria apenas para o Home?
+export type Food = {
+  id: number
+  titulo: string
+  tags: {
+    destacado?: boolean
+    tipo: string
+  }
+  avaliacao: number
+  descricao: string
+  capa: string
+}
+
+const PerfilProduct = () => {
+  return (
+    <Container>
+      <div className="container">
+        <List>
+          <li>
+            <Card>
+              <PizzaImg src={pizza} alt="Pizza" />
+              <PizzaName>Pizza Marguerita</PizzaName>
+              <Description>
+                A clássica Marguerita: molho de tomate suculento, mussarela
+                derretida, manjericão fresco e um toque de azeite. Sabor e
+                simplicidade!
+              </Description>
+              <Button href="#">Adicionar ao carrinho</Button>
+            </Card>
+          </li>
+          <li>
+            <Card>
+              <PizzaImg src={pizza} alt="Pizza" />
+              <PizzaName>Pizza Marguerita</PizzaName>
+              <Description>
+                A clássica Marguerita: molho de tomate suculento, mussarela
+                derretida, manjericão fresco e um toque de azeite. Sabor e
+                simplicidade!
+              </Description>
+              <Button href="#">Adicionar ao carrinho</Button>
+            </Card>
+          </li>
+          <li>
+            <Card>
+              <PizzaImg src={pizza} alt="Pizza" />
+              <PizzaName>Pizza Marguerita</PizzaName>
+              <Description>
+                A clássica Marguerita: molho de tomate suculento, mussarela
+                derretida, manjericão fresco e um toque de azeite. Sabor e
+                simplicidade!
+              </Description>
+              <Button href="#">Adicionar ao carrinho</Button>
+            </Card>
+          </li>
+          <li>
+            <Card>
+              <PizzaImg src={pizza} alt="Pizza" />
+              <PizzaName>Pizza Marguerita</PizzaName>
+              <Description>
+                A clássica Marguerita: molho de tomate suculento, mussarela
+                derretida, manjericão fresco e um toque de azeite. Sabor e
+                simplicidade!
+              </Description>
+              <Button href="#">Adicionar ao carrinho</Button>
+            </Card>
+          </li>
+          <li>
+            <Card>
+              <PizzaImg src={pizza} alt="Pizza" />
+              <PizzaName>Pizza Marguerita</PizzaName>
+              <Description>
+                A clássica Marguerita: molho de tomate suculento, mussarela
+                derretida, manjericão fresco e um toque de azeite. Sabor e
+                simplicidade!
+              </Description>
+              <Button href="#">Adicionar ao carrinho</Button>
+            </Card>
+          </li>
+          <li>
+            <Card>
+              <PizzaImg src={pizza} alt="Pizza" />
+              <PizzaName>Pizza Marguerita</PizzaName>
+              <Description>
+                A clássica Marguerita: molho de tomate suculento, mussarela
+                derretida, manjericão fresco e um toque de azeite. Sabor e
+                simplicidade!
+              </Description>
+              <Button href="#">Adicionar ao carrinho</Button>
+            </Card>
+          </li>
+        </List>
+      </div>
+    </Container>
+  )
+}
 
 export default PerfilProduct
